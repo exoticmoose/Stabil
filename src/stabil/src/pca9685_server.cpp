293c96bdb435a3a1492ba27f146e0e3a1b3dbf0e
exoticmoose <paul.schneider@email.ucr.edu>
2019-02-22 01:37:37 -0800

    JHPWMDRiver inclusion

--- src/stabil/src/pca9685_server.cpp
+++ src/stabil/src/pca9685_server.cpp
@@ -1,12 +1,13 @@
 #include "ros/ros.h"
 #include "stabil/PCA9685.h"
+#include "JHPWMPCA9685.h"
+
+PCA9685 *pca9685;
 
 bool setServo(stabil::PCA9685::Request &req, stabil::PCA9685::Response &res) {
-	ROS_INFO("Got request to set servo %d to %d", 69, 240);
-
+	ROS_INFO("Got request to set servo %d to %d", req.address, req.request);
+	pca9685->setPWM(req.address, 0, req.request);
 	return true;
-
-
 
 }
 
@@ -15,6 +16,23 @@ int main(int argc, char **argv) {
 	ros::NodeHandle n;
 
 	ros::ServiceServer service = n.advertiseService("pca_9685", setServo);
+
+	pca9685 = new PCA9685();
+	int err = pca9685->openPCA9685();
+	if (err < 0) {
+		ROS_ERROR("Unable to open PCA9685 Connection");
+	}
+	else {
+		  pca9685->setAllPWM(0,0);
+		  pca9685->reset();
+		  pca9685->setPWMFrequency(250);
+		  ROS_INFO("PCA9685 Connection Successful.");
+	}
+
+
+
+
+
 	ROS_INFO("Ready to roll!");
 	ros::spin();
 
